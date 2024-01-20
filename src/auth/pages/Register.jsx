ee31eb907ec6a22239bdf2a941b5bfed20639e10
Jymma <jkmogollon.cp@gmail.com>
2024-01-20 00:21:57 -0500

    Add Login and Register

--- src/auth/pages/Register.jsx
+++ src/auth/pages/Register.jsx
@@ -1,3 +1,52 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="Name" type="text" placeholder="Name" fullWidth />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Add account
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>
+              Do you already have an account?
+            </Typography>
+            <Link component={RouterLink} color="primary.main" to="/auth/login">
+              Get into
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
